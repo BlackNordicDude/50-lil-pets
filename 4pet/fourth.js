3b3bc94333da832ad1f1e9b1716a7a9c841a1e7a
BlackNordicDude <48724415+BlackNordicDude@users.noreply.github.com>
2023-03-26 22:12:05 +0300

    completed 4 pet

---
+++ 4pet/fourth.js
@@ -0,0 +1,27 @@
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const i = document.querySelector('.fa-search');
+
+const closeSearch = () => {
+    btn.removeEventListener('click', openSearch)
+}
+
+const openSearch = () => {
+    search.classList.add('active');
+    input.focus()
+    return closeSearch
+}
+
+btn.addEventListener('click', openSearch)
+
+console.log(i, btn, search);
+
+document.addEventListener('click', (e) => {
+    console.log(e.target);
+    if (e.target === input || e.target === i || e.target === btn) {
+        console.log('OK');
+    } else {
+        search.classList.remove('active')
+    }
+})
