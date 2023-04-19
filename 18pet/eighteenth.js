3cd2dbf5c4a600364158da7b29a43499af30ac06
BlackNordicDude <48724415+BlackNordicDude@users.noreply.github.com>
2023-04-19 10:21:56 +0300

    completed 18 pet

---
+++ 18pet/eighteenth.js
@@ -0,0 +1,36 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let currentSlide = 0;
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[currentSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'))
+    slides[currentSlide].classList.add('active')
+}
+
+rightBtn.addEventListener('click', () => {
+    currentSlide++
+    if (currentSlide > slides.length - 1) {
+        currentSlide = 0
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    currentSlide--
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1
+    }
+    setBgToBody()
+    setActiveSlide()
+
+})
